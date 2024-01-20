08fbd688759cf58271872585a58ccc65c1866e44
richa1101 <109916564+richa1101@users.noreply.github.com>
2024-01-20 17:49:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+// dice1.png - dice6.png
+
+var randomImageSource = "image/" + randomDiceImage;  // images/dice1.png - image/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+//var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "image/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//if player 1 wins
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = " 🚩Player 1 Wins!";
+}
+else if(randomNumber2 > randomNumber1)
+{
+    document.querySelector("h1").innerHTML = " Player 2 Wins!🚩"
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
